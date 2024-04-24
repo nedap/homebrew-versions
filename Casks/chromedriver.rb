523f9f4e9083e16e3214a3cb89cd3c65d93cb4ca
mvrijsen <152891651+mvrijsen@users.noreply.github.com>
2024-04-24 11:34:35 +0200

    Update chromedriver.rb
    Modify appcast to livecheck

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -4,7 +4,7 @@ cask 'chromedriver' do
   
     # chromedriver.storage.googleapis.com was verified as official when first introduced to the cask
     url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
-    appcast 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+    livecheck 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
     name 'ChromeDriver'
     homepage 'https://sites.google.com/a/chromium.org/chromedriver/home'
     binary 'chromedriver'
